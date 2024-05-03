7c0b63b9a7b06ccddf7ce6f78ed78c444a62ea6f
Ismat-Samadov <ismetsemedov@gmail.com>
2024-05-03 16:08:37 +0400

    adding template

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,60 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "A new kitten has been created!"
+      redirect_to @kitten
+    else
+      flash.now[:error] = "You fool, kittens can't be created like that!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      flash[:success] = "The kitten has been edited!"
+      redirect_to @kitten
+    else
+      flash.now[:error] = "You fool, you can't edit a kitten like that!"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:success] = "Bye bye #{@kitten.name}"
+    redirect_to kittens_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
